7bc73363c59affa781d5f5320e0560668350e49c
eduboi042 <erica_dubois@comcast.com>
2023-10-09 09:53:24 -0600

    timer working on game page

--- src/views/Player.js
+++ src/views/Player.js
@@ -1,6 +1,7 @@
 import { Lightning, VideoPlayer } from '@lightningjs/sdk';
 
-const videoUrl = 'videos/Ad1a.mov';
+const videoUrl1 = 'videos/Ad1a.mov';
+const videoUrl2 = 'videos/Ad2a.mov';
 const timeout = 15000;
 export default class Player extends Lightning.Component {
   static _template() {
@@ -15,23 +16,34 @@ export default class Player extends Lightning.Component {
   _firstActive() {
     console.log(VideoPlayer);
     VideoPlayer.consumer(this);
-    VideoPlayer.open(videoUrl);
+    VideoPlayer.open(videoUrl1);
     console.log(VideoPlayer);
     VideoPlayer.show();
+    VideoPlayer.loop(false);
   }
   _handleEnter() {
     VideoPlayer.playPause();
   }
 
+  _playPause() {
+    VideoPlayer.playPause();
+  }
   hidePlayer() {
     VideoPlayer.playPause();
     VideoPlayer.hide();
   }
 
-  showPlayer() {
-    VideoPlayer.open(videoUrl);
+  showPlayer1() {
+    VideoPlayer.open(videoUrl1);
     VideoPlayer.show;
     VideoPlayer.playPause();
+    VideoPlayer.loop(false);
+  }
+
+  showPlayer2() {
+    VideoPlayer.open(videoUrl2);
+    VideoPlayer.show;
+    VideoPlayer.loop(false);
   }
 }
 
